2de4ae14f7e82ee7075e9a7ebc91e92f99a39cf2
Jason Leatiota <jasole2007@yahoo.com>
2021-05-29 21:44:25 -0700

    reorganizing

---
+++ quotes/wlQuotes.js
@@ -0,0 +1,411 @@
+const fs = require('fs');
+
+
+//*WEIGHT LOSS MOTIVATION QUOTES*
+
+const sectionOne = 
+`The best project you’ll ever work on is YOU.
+
+Look in the mirror: that’s your competition.
+
+You said tomorrow yesterday.
+
+Decide, Commit, Succeed.
+
+No excuses; Just results.
+
+You don’t find will power, you create it.
+
+Your largest fear carries your greatest growth.
+
+Nothing tastes as good as skinny feels.
+
+The body achieves what the mind believes.
+
+If you’re tired of starting over, stop giving up.
+
+Three months from now, you will thank yourself.
+
+It’s not just the weight you lose: but the life you gain.
+
+A healthy outside starts from the inside. - Robert Urich
+
+Push yourself because, no one else is going to do it for you.
+
+Once you control your mind you can conquer your body.
+
+When you find your why, you find a way to make it happen. - Eric Thomas
+
+If it is important to you, you will find a way; If not, you’ll find an excuse.
+
+Losing weight is hard; Maintaining weight is hard; Staying overweight is hard; Choose your hard.`
+
+//INSPIRATIONAL WEIGHT LOSS QUOTES
+
+const sectionTwo = 
+
+`Your only limit is you.
+
+It never gets easier. You just get better. - Jordan Hoechlin
+
+You aren’t fat. You have fat. Fat does not define you.
+
+It is the things we work hardest for that will reward us the most.
+
+Being healthy isn’t about the weight you lose, it’s about the life you gain.
+
+If you want to get fit, you have to give away only one thing. Your excuses.
+
+The struggle you’re in today is developing the strength you need for tomorrow.
+
+Discipline is choosing between what you want now and what you want most.
+
+You are not just doing it for yourself. Your transformation can inspire others.
+
+If you want something you’ve never had, then you’ve got to do something you’ve never done.
+
+You’ve always been beautiful. Now you’re just deciding to be healthier, fitter, faster and stronger. Remember that.
+
+There are two primary choices in life: to accept conditions as they exist, or accept the responsibility for changing them. - Dr. Denis Waitley
+
+To lose weight permanently requires faith - faith there is a bigger life waiting for you beyond food and weight. - Catherine L. Taylor
+
+Strength doesn’t come from what you can do. It comes from overcoming the things you once thought you couldn’t. - Nikki Rogers
+
+There are only two options regarding commitment; you’re either in or you’re out. There’s no such thing as life in-between. - Pat Riley
+
+The reason why people give up so fast is because they tend to look at how far they still have to go, instead of how far they have gotten.
+
+Obstacles don’t have to stop you. If you run into a wall, don’t turn around and give up. Figure out how to climb it, go through it, or work around it. - Michael Jordan`
+
+//DIET MOTIVATION QUOTES
+
+const sectionThree =
+
+`Unless you puke, faint, or die, keep going. - Jillian Michaels
+
+Wake up with determination. Go to bed with satisfaction.
+
+Success is what comes after you stop making excuses. - Luis Galarza
+
+Eat for the body you want, not for the body you have.
+
+It is not easy, but it is worth it. Now repeat that everyday.
+
+One workout at a time. One day at a time. One meal at a time.
+
+Junk food satisfies you for a minute. Being fit satisfies you for life.
+
+You were strong enough to get this far. You are strong enough to keep going.
+
+Just 21 days, 21 days of healthy eating and working out, and it will become habit.
+
+In two weeks, you’ll feel it. In four weeks, you’ll see it. In eight weeks, you’ll hear it.
+
+Your body can stand up to almost anything. It’s your mind that you have to convince.
+
+Fattening food satisfies you for a few minutes … Feeling good about yourself lasts forever.
+
+The first step to getting what you want is having the courage to get rid of what you don’t.
+
+Most people fail, not because of lack of desire, but, because of lack of commitment. - Vince Lombardi
+
+Junk food you’ve craved for an hour, or the body you’ve craved for a lifetime? Your decision.
+
+It took more than a day to put it on. It will take more than a day to take it off. Keep going.
+
+I will NOT feel deprived when I bypass junk food. I will feel empowered that I made the healthy choice.
+
+If I quit now I will soon be back to where I started. And when I started, I was desperate to get to where I am now.`
+
+//INSPIRATIONAL DIET QUOTES
+
+const sectionFour =
+
+`This month’s diet is next month’s body.
+
+Don’t let a bad day turn into a bad week.
+
+Nothing tastes as good as healthy feels.
+
+It is not the mountain we conquer, but ourselves. - Sir Edmund Hillary
+
+I may not be there yet, but I’m closer than I was yesterday.
+
+Every small achievement will soon be one amazing success.
+
+Wanting something is not enough. You must hunger for it. - Les Brown
+
+Do something now; your future self will thank you for later.
+
+Suffer the pain of self-discipline. Or suffer the pain of regret. - Jim Rohn
+
+You have to be at your strongest when you’re feeling at your weakest.
+
+Your diet is a bank account. Good food choices are good investments. - Bethenny Frankel
+
+Never stop trying. Never stop believing. Never give up. Your day will come.
+
+I’m not telling you it is going to be easy, I’m telling you it’s going to be worth it. - Art Williams
+
+Fear says… no you can’t. Determination says… yes you can. Results say… I told you so.
+
+Everything you can imagine about how you will look and feel when you lose weight is real. - Felicity Luckey
+
+Sometimes you don’t realize your own strength until you come face to face with your greatest weakness. - Susan Gale`
+
+//POSITIVE WEIGHT LOSS QUOTES
+
+const sectionFive = 
+
+`Believe in new possibilities.
+
+You are stronger than you think.
+
+Once you choose hope, anything’s possible. - Christopher Reeve
+
+In the middle of a difficulty lies opportunity. - Albert Einstein
+
+Believe you can and you’re halfway there. - Theodore Roosevelt
+
+A little progress each day adds up to big results. - Satya Nani
+
+Every morning you wake up is another chance to get it right.
+
+Your body hears everything your mind says. Stay positive.
+
+What you tell yourself every day will either lift you up or tear you down.
+
+Don’t think about what might go wrong. Think about what might go right.
+
+Never stop trying. Never stop believing. Never give up. Your day will come.
+
+Results happen over time, not overnight. Work hard, stay consistent, and be patient.
+
+If you believe it will work out, you’ll see opportunities. If you believe it won’t, you will see obstacles. - Wayne Dyer`
+
+//WEIGHT LOSS GOALS QUOTES
+
+const sectionSix =
+
+`If the plan doesn’t work, change the plan but never the goal.
+
+Focus on your goal. Don’t look in any direction but ahead.
+
+You don’t get what you wish for. You get what you work for.
+
+Be stubborn about your goals and flexible about your methods.
+
+Discipline is the bridge between goals and accomplishments. - Jim Rohn
+
+You don’t want to look back on your life and know you could have done better.
+
+Set a goal so big that you can’t achieve it until you grow into the person who can.
+
+The pain of the process is only temporary, but the feeling of achievement lasts forever.
+
+Weight loss doesn’t begin in the gym with a dumb bell; it starts in your head with a decision. - Toni Sorenson
+
+Only put off starting a weight loss program until tomorrow if you are willing to die without having achieved your weight loss goals. - Felicity Luckey
+
+Self-discipline is about controlling your desires and impulses while staying focused on what needs to get done to achieve your goal. - Adam Sicinski
+
+One of the most important keys to success and achieving your goals is having the discipline to do what you know you should do, even when you don’t feel like doing it.`
+
+const subSectionSixOne =
+
+`No more negative body thoughts.
+No more “I’ll do it tomorrow”.
+No more sitting and wishing for a thinner me.
+No more eating when I am not hungry.
+No more waiting for this to get easier.
+No more muffin tops,
+No more wobbly thighs.
+No more soft, round stomach,
+No more “Buts…”.
+No more “I can’t”.
+No more “It’s too hard”.
+No more “I’m too tired”.
+No more excuses.`
+
+//NEVER GIVE UP WEIGHT LOSS QUOTES
+
+const sectionSeven = 
+
+`Don’t stop until you’re proud.
+
+Pain is temporary. Quitting lasts forever.
+
+Don’t give up on yourself. You’re worth it.
+
+Tough times don’t last. Tough people do. - Robert H. Schuller
+
+Hope never abandons you, you abandon it. - George Weinberg
+
+When you feel like quitting, think about why you started.
+
+Be strong when you are weak & brave when you are scared.
+
+Never stop trying. Never stop believing. Never give up. Your day will come.
+
+Determination is doing what needs to be done even when you don’t feel like doing it.`
+
+const subSectionSevenOne = 
+
+`Never quit.
+If you stumble get back up.
+What happened yesterday no longer matters.
+Today’s another day.
+So get back on track and
+Move closer to your dreams and goals.
+You can do it.`
+
+// FUNNY WEIGHT LOSS QUOTES
+
+const sectionEight =
+
+`Eat less sugar, you’re sweet enough already.
+
+Do not reward yourself with food, you’re not a dog.
+
+Suck it up and one day you won’t have to suck it in.
+
+Note to myself: When I eat like crap, I feel like crap.
+
+Dear Stomach, you’re bored, not hungry. So shut up.
+
+You are what you eat - so don’t be Fast, Cheap, Easy, or Fake.
+
+No matter how slow you go, you are still lapping everybody on the couch.
+
+You can’t expect to look like a million bucks if you eat from the dollar menu.
+
+I’m not losing weight. I’m getting rid of it. I have no intention of finding it again.
+
+Your body is the baggage you must carry through life. The more excess the baggage, the shorter the trip. - Arnold H. Glasgow
+
+Saying “Oh, I’ve already ruined my good eating today. I’ll just eat crap” is like saying “Oh, I dropped my phone on the floor. I’ll just smash it till it breaks.”`
+
+// DIET & WEIGHT LOSS SAYINGS
+
+const sectionNine = 
+
+`What you eat in private eventually is what you wear in public. Eat clean, look lean.
+
+Junk food you’ve craved for an hour, or the body you’ve craved for a lifetime? Your decision.
+
+Those who think they have no time for healthy eating will sooner or later have to find time for illness.
+
+Respect your body. Fuel your body. Challenge your body. Move your body. And most of all, love your body.
+
+Even if you can’t physically see the results in front of you, every single effort is changing your body from the inside.
+
+If you are really serious about losing weight, you need to be completely honest with yourself about what you’re eating.
+
+Losing weight is a mental challenge, not a physical one. If you can take control of your mind and body, the weight loss will follow.
+
+While weight loss is important, what’s more important is the quality of food you put in your body - food is information that quickly changes your metabolism and genes. - Dr. Mark Hyman
+
+When you look in the mirror and see no change, and still keep faith, knowing that in time you will get there if you stay focused and on track, that’s the difference between those who succeed and those who fail.
+
+Weight loss can change your whole character. That always amazed me: Shedding pounds does change your personality. It changes your philosophy of life because you recognize that you are capable of using your mind to change your body. - Jean Nidetch
+
+Exercise to be fit, not skinny. Eat to nourish your body and always ignore the haters, doubters and unhealthy examples that were once feeding you. You are worth more than you realize.
+
+There’s a difference between interest and commitment. When you’re interested in doing something, you do it only when it’s convenient. When you’re committed to something, you accept no excuses; only results. - Kenneth Blanchard
+
+Remember how your clothes don’t fit, your body feels sluggish, your skin doesn’t glow, you don’t have energy, you want to feel beautiful, you’re regretting what you ate, you’re wishing you worked out, you wanted to change, before making another unhealthy choice.
+
+Want to lose weight? Do more than just think about it. You don’t need to hold off while you keep searching for an abundance of motivation to lose weight. What you need is an abundance of action! - Shawn Weeks
+
+Dear Me,
+I am not going to lose all this weight in a week. Heck, I probably won’t even lose it in a month. But, I am going to start eating great and working out for the rest of my life, not one week or just one month.
+
+Don’t wait until everything is just right. It will never be perfect. There will always be challenges, obstacles and less than perfect conditions. So what. Get started now. With each step you take, you will grow stronger and stronger, more and more skilled, more and more self-confident and more and more successful. - Mark Victor Hansen
+
+When your body absorbs toxins, it stores them in fat, which is why fiber and probiotics are strategic weapons for weight loss. Fiber keeps your colon healthy and reduces your body’s absorption of toxins. - Suzanne Somers`
+
+const sectionTen =
+
+`Dear Body,
+I’m sorry I’ve treated you this way, feeding you the wrong foods and not taking care of you. I promise to do better and get you back to the best shape and fitness level you can be. We can do it!
+Sincerely,
+Me.`
+
+
+function makeQuoteObj(singleLineStrings){
+    const linesArray = singleLineStrings.split("\n\n");
+    // console.log('first split: ', linesArray);
+    const nestedLineAuthor = linesArray.map(line => line.split(' - '))
+    // console.log('second split: ', nestedLineAuthor);
+
+    const removedEmpties = nestedLineAuthor.filter(arr => arr.length > 0)
+    // console.log('third step: ', removedEmpties)
+    const makeNewQuoteObj = removedEmpties.map(nestedArray => {
+        const temp = (nestedArray.length > 1) ? `"${nestedArray[1]}"` : null;
+        
+        const quote = `{Author: ${temp}, Quote: "${nestedArray[0]}"}`
+        return quote;
+    })
+    // console.log('last step: ', makeNewQuoteObj)
+    const blah = makeNewQuoteObj.join(',\n')
+    return blah
+}
+const arrToStr = arr => {
+    let str = ""
+
+    arr.forEach( obj => {
+        str += obj + ",\n"
+    })
+    return str;
+}
+
+
+function multiLineQuote(string){
+    const quote = `{Author: "Anonymous", Quote: "${string}"}`
+    // console.log(quote)
+    return quote;
+}
+// console.log(finale(makeQuoteObj(sectionOne)))
+const fquotes = arrToStr([
+    (makeQuoteObj(sectionOne)),
+    (makeQuoteObj(sectionTwo)),
+    (makeQuoteObj(sectionThree)),
+    (makeQuoteObj(sectionFour)),
+    (makeQuoteObj(sectionFive)),
+    (makeQuoteObj(sectionSix)),
+    (makeQuoteObj(sectionSeven)),
+    (makeQuoteObj(sectionEight)),
+    (makeQuoteObj(sectionNine)),
+    ])
+
+const fquotes2 = "" + multiLineQuote(subSectionSixOne) + ",\n" + multiLineQuote(subSectionSevenOne) + ",\n" + multiLineQuote(sectionTen)
+
+const printThis = fquotes + ",\n" + fquotes2
+// console.log(fquotes)
+
+// console.log(fquotes)
+// console.log(finale(makeQuoteObj(sectionOne)))
+
+// const printThis = finale(makeQuoteObj(sectionOne))
+
+fs.writeFileSync('./Quotes1.js', printThis, (err) => {
+    if (err) throw err
+    console.log('completed')
+})
+
+// fs.writeFileSync('./Quotes1.js',  (err) => {    
+//     if (err) throw err;
+//     console.log('Quotes written and saved!');
+// });
+// const allQuotes = [sOne, sTwo, sThree, sFour, sFive, sSix, sSixOne, sSeven, sSevOne, sEight, sNine, sTen]
+// const jsoned = []
+// const allQuotesJson = allQuotes.map(set => set.forEach(quote => jsoned.push(JSON.stringify(quote))))
+// const final = JSON.stringify(allQuotesJson)
+
+
+module.exports =    {
+                    makeQuoteObj,
+                    arrToStr,
+                    multiLineQuote
+                    }
