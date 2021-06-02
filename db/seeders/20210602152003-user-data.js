18e96c389f69bfc6f52b3bdf606605ba2cd89330
Jason Leatiota <jasole2007@yahoo.com>
2021-06-02 11:24:01 -0700

    lunch break

--- db/seeders/20210602152003-user-data.js
+++ db/seeders/20210602152003-user-data.js
@@ -1,13 +1,26 @@
-'use strict';
-const bcrypt = require('bcrypts')
+"use strict";
+const bcrypt = require("bcryptjs");
+const { getHash } = require("../../routes/utils");
 
+const pword = getHash("password", 10);
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-
-    return queryInterface.bulkInsert('Users', [
-      {fullName: 'Jon Doe', email: 'fake@email.com', bodyWeight: '194', bodyFatPercentage: 12}
-    ])
+    return queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          fullName: "Jon Doe",
+          email: "fake@email.com",
+          bodyWeight: "194",
+          bodyFatPercentage: 12,
+          hashedPassword: pword,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+      ],
+      {}
+    );
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -21,12 +34,12 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('People', null, {});
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkDelete('People', null, {});
     */
-  }
+  },
 };
