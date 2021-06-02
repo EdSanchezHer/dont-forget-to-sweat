4105825d8e37c03f184c7953c577ba081746e69c
Khiienu <anthonyhuynh123@gmail.com>
2021-06-02 11:45:07 -0500

    created util folder, started on routes for user

---
+++ routes/utils.js
@@ -0,0 +1,11 @@
+const csrf = require('csurf')
+
+const csrfProtection = csrf({cookie : true})
+
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch((e) => next(e))
+
+
+module.exports = {
+    csrfProtection,
+    asyncHandler
+}
