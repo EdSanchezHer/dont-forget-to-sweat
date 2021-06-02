e0a63ad72cf307569877f6e86df38f3cc9f0697a
Huy Lam <commanderh1995@gmail.com>
2021-06-02 15:20:18 -0500

    add stuff

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-	res.render("login", { title: "a/A Express Skeleton Home" });
+	res.render("signup", { title: "a/A Express Skeleton Home" });
 });
 
 module.exports = router;
