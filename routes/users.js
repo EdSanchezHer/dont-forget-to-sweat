400edad121f4797dc79c4c00098580825b1878bf
Jason Leatiota <jasole2007@yahoo.com>
2021-06-03 16:43:54 -0700

    asdf

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const { csrfProtection, asyncHandler, getHash, isPassword } = require("./utils")
 const db = require("../db/models");
 const { render } = require('pug');
 const { check, validationResult } = require("express-validator");
-const user = require('../db/models/user');
+// const user = require('../db/models/user');
 
 
 const loginUser = (req, res, next) => {
@@ -19,66 +19,59 @@ const logoutUser = (req, res) => {
 };
 //TITLE: START OF VALIDATORS
 
-const userValidator = [
-  check("fullName")
-  .exists({ checkFalsey: true})
-  .withMessage("Enter full name please")
-  .isLength({max: 75})
-  .withMessage("Full name can not be more than 75 characters long"),
+// const userValidator = [
+//   check("fullName")
+//   .exists({ checkFalsey: true})
+//   .withMessage("Enter full name please")
+//   .isLength({max: 75})
+//   .withMessage("Full name can not be more than 75 characters long"),
   
-  check("email")
-  .exists({checkFalsey: true})
-  .withMessage("Enter an email please")
-  .isLength({max: 75})
-  .withMessage("Email can not be more than 75 characters long")
-  .isEmail()
-  .withMessage("Email address is not a valid email")
-  .custom((value) => {
-    return db.User.findOne({where: {email: value}}).then((user) => {
-      if(user) {
-        return Promise.reject(
-          "The Email given is already used by another account"
-          )
-        }
-      })
-    }),
-    check("password")
-    .exists({ checkFalsey: true })
-    .withMessage("Please enter a password")
-    .isLength({ min: 8})
-    .withMessage("Password must be at least 8 characters long")
-    .isLength({ max : 20 })
-    .withMessage("Password can not exceed 20 characters")
-    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, "g")
-    .withMessage('Password must contain one lowercase letter, one uppercase letter, number, and a special character ("!@#$%^&*")')
-  ];
+//   check("email")
+//   .exists({checkFalsey: true})
+//   .withMessage("Enter an email please")
+//   .isLength({max: 75})
+//   .withMessage("Email can not be more than 75 characters long")
+//   .isEmail()
+//   .withMessage("Email address is not a valid email")
+//   .custom((value) => {
+//     return db.User.findOne({where: {email: value}}).then((user) => {
+//       if(user) {
+//         return Promise.reject(
+//           "The Email given is already used by another account"
+//           )
+//         }
+//       })
+//     }),
+//     check("password")
+//     .exists({ checkFalsey: true })
+//     .withMessage("Please enter a password")
+//     .isLength({ min: 8})
+//     .withMessage("Password must be at least 8 characters long")
+//     .isLength({ max : 20 })
+//     .withMessage("Password can not exceed 20 characters")
+//     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, "g")
+//     .withMessage('Password must contain one lowercase letter, one uppercase letter, number, and a special character ("!@#$%^&*")')
+//   ];
   
-  const loginValidator = [
-    check("email")
-    .exists({ checkFalsey: true})
-    .withMessage("Please enter your email"),
-    check("password")
-    .exists({ checkFalsey: true})
-    .withMessage("Please enter your password")
-  ];
+//   const loginValidator = [
+//     check("email")
+//     .exists({ checkFalsey: true})
+//     .withMessage("Please enter your email"),
+//     check("password")
+//     .exists({ checkFalsey: true})
+//     .withMessage("Please enter your password")
+//   ];
   
   //TITLE: END of Validators
   
   //* START of routes
   /* GET users listing. */
-  // router.get('/', function(req, res, next) {
-  //   res.send('respond with a resource');
-  // });
-  
-  router.get("/signup", csrfProtection, asyncHandler(async (req, res) => {
-  const user = await db.User.build({
-    fullName: null,
-    email: null,
-    hashedPassword: null,
-    bodyWeight: null,
-    bodyFatPercentage: null,
-    fitnessLevel: null
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
   });
+  
+router.get("/signup", csrfProtection, asyncHandler(async (req, res) => {
+  const user = db.User.build();
 
   res.render("signup", {
     title: "Sign Up",
@@ -90,35 +83,29 @@ const userValidator = [
 router.post(
   "/signup",
   csrfProtection,
-  userValidator,
-  asyncHandler(async(req, res) => {
-    const {fullName, email, password} = req.body;
+  // userValidator,
+  asyncHandler(async (req, res) => {
+    const { fullName, email, password } = req.body;
 
-    const user = await db.User.build({
-      fullName,
-      email,
-      password,
-      bodyWeight: null,
-      bodyFatPercentage: null,
-      fitnessLevel: null,
-    })
+    const newUser = db.User.build({ fullName, email })
 
-    const validatorError = validationResult(req);
+    // const validatorErrors = validationResult(req);
 
-    if(validatorError.isEmpty()) {
-      user.hashedPassword = getHash(user.password, 8)
-      await user.save();
+    // if(validatorErrors.isEmpty()) {
+      newUser.hashedPassword = getHash(password, 8)
+      await newUser.save();
       loginUser(req, res, user);
       res.redirect("/app")
-    } else {
-      const errors = validatorError.array().map((error) => error.msg);
-      res.render("signup", {
-        title: "Sign up",
-        user,
-        errors,
-        csrfToken: req.csrfToken(),
-      })
-    }
+    // } else {
+    //   const errors = validatorErrors.array().map((error) => error.msg);
+    //   res.render('error', {csrfToken: req.csrfToken})
+    //   // res.render("signup", {
+      //   title: "Sign up",
+      //   user,
+      //   errors,
+      //   csrfToken: req.csrfToken(),
+      
+    // }
   })
 )
 
@@ -131,13 +118,13 @@ router.get("/login", csrfProtection, (req, res, next)=> {
   router.post(
     "/login",
     csrfProtection,
-    loginValidator,
+    // loginValidator,
     asyncHandler(async(req, res) => {
       const { email, password } = req.body;
       const errors = [];
-      const validatorError = validationResult(req)
+      // const validatorErrors = validationResult(req)
 
-      if(validatorError.isEmpty()) {
+      // if(validatorErrors.isEmpty()) {
         const user = await db.User.findOne({ where: { email } });
         if(user !== null) {
           const passwordMatch = await isPassword(password, user.hashedPassword.toString());
@@ -146,10 +133,10 @@ router.get("/login", csrfProtection, (req, res, next)=> {
               return res.redirect("/app");
             }
         }
-          errors.push("Login failed for the provided email address and password")
-      } else {
-        errors = validatorError.array().map((error) => error.msg);
-      }
+          // errors.push("Login failed for the provided email address and password")
+      // } else {
+        // errors = validatorErrors.array().map((error) => error.msg);
+      // }
       res.render("login", {
         title: "Login",
         errors,
