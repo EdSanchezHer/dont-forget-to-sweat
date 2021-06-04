860ac5c7f71946263559d87f8be8ff9f461d7010
Khiienu <anthonyhuynh123@gmail.com>
2021-06-04 09:27:06 -0500

    completed routes, and did auth

--- routes/landing.js
+++ routes/landing.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const { requireAuth } = require("../auth");
 const router = express.Router();
 // const { requireAuth } = require("../auth")
-router.get("/gym", (req, res) => {
+router.get("/gym", requireAuth, (req, res) => {
     res.render("gym");
+})
+
+router.post("/users/logout", (req, res) => {
+    
 })
 module.exports = router
