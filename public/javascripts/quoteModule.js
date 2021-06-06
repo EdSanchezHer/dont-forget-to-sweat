857da6672e0e88a1dafd2da2382ad92fd75add68
Eduardo <eduardosh1105@gmail.com>
2021-06-06 12:14:35 -0600

    Quotes are working

--- public/javascripts/quoteModule.js
+++ public/javascripts/quoteModule.js
@@ -1,55 +1,52 @@
 class NewQuote {
-    constructor() {
-        this.time = new Date(),
-        this.max = 247
-    }
+	constructor() {
+		(this.time = new Date()), (this.max = 247);
+	}
 
-    getRandomQuoteId() {
-        return Math.floor(Math.random() * this.max);
-    }
+	getRandomQuoteId() {
+		return Math.floor(Math.random() * this.max);
+	}
 
-    lineBreaker(longQuote) {
+	lineBreaker(longQuote) {
+		let firstLine;
+		let remainingQuote;
+		if (longQuote[50] === " ") {
+			firstLine = longQuote.slice(0, 50);
+			remainingQuote = longQuote.slice(51);
+		} else {
+			let index;
+			let found = false;
+			let x = 49;
+			let y = 51;
+			while (!found) {
+				if (longQuote[x] === " ") {
+					index = x;
+					found = true;
+				}
+				if (longQuote[y] === " ") {
+					index = y;
+					found = true;
+				}
+				x--;
+				y++;
+			}
+			firstLine = longQuote.slice(0, index);
+			remainingQuote = longQuote.slice(index);
+			if (remainingQuote.length >= 50) {
+				return ` ${firstLine} \n ${lineBreaker(remainingQuote)}`;
+			}
+		}
+	}
+	formatLongQuote() {
+		const author = this.author;
+		const quote = this.quote;
 
-        let firstLine;
-        let remainingQuote;
-        if (longQuote[50] === ' ') {
-            firstLine = longQuote.slice(0, 50)
-            remainingQuote = longQuote.slice(51)
-        } else {
-            let index;
-            let found = false
-            let x = 49
-            let y = 51
-            while (!found) {
-                if (longQuote[x] === ' ') {
-                    index = x
-                    found = true;
-                }
-                if (longQuote[y] === ' ') {
-                    index = y;
-                    found = true;
-                }
-                x--
-                y++
-            }
-            firstLine = longQuote.slice(0, index);
-            remainingQuote = longQuote.slice(index)
-            if (remainingQuote.length >= 50) {
-                return ` ${firstLine} \n ${lineBreaker(remainingQuote)}`
-            }
-        }
+		if (author === null) quoteObj.author = "Anonymous";
 
-    }
-    formatLongQuote() {
-        const author = this.author;
-        const quote = this.quote
+		if (quote.length > 75) quoteObj.quote = lineBreaker(quote);
 
-        if (author === null) quoteObj.author = "Anonymous"
-
-        if (quote.length > 75) quoteObj.quote = lineBreaker(quote);
-
-        return quoteObj;
-    }
+		return quoteObj;
+	}
 }
 
-module.exports = NewQuote
+module.exports = NewQuote;
--- routes/quotes.js
+++ routes/quotes.js
@@ -1,19 +1,20 @@
-const express = require('express')
-const router = express.Router()
-const { asyncHandler, csrfProtection } = require('./utils')
-const db = require('../db/models')
-const Quote = require('../public/javascripts/quoteModule')
+const express = require("express");
+const router = express.Router();
+const { asyncHandler, csrfProtection } = require("./utils");
+const db = require("../db/models");
+const Quote = require("../public/javascripts/quoteModule");
 
-router.get("/", asyncHandler(async (req, res, next) => {
-    const NewQuote = new Quote();
-    const quoteId = NewQuote.getRandomQuoteId()
-    const newQ = await db.Quote.findByPk(quoteId)
+router.get(
+	"/",
+	asyncHandler(async (req, res, next) => {
+		const NewQuote = new Quote();
+		const quoteId = NewQuote.getRandomQuoteId();
+		const newQ = await db.Quote.findByPk(quoteId);
 
-    const formatedQuote = await newQ.formatLongQuote()
-    console.log(formatedQuote
-        )
-    res.json({ formatedQuote })
+		// const formattedQuote = await newQ.formatLongQuote();
+		// console.log(newQ);
+		res.json(newQ);
+	})
+);
 
-}))
-
-module.exports = router
+module.exports = router;
